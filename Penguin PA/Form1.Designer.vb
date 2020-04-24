45b5ef04eb8b0465ac32f996e6f7b3ca68b47c0a
julioyosafat98 <julioyosafat987@gmail.com>
2020-04-24 18:05:26 +0700

    Nice

--- Penguin PA/Form1.Designer.vb
+++ Penguin PA/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,37 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrAnimation = New System.Windows.Forms.Timer(Me.components)
+        Me.PicPenguin = New System.Windows.Forms.PictureBox()
+        CType(Me.PicPenguin, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PicPenguin
+        '
+        Me.PicPenguin.BackgroundImage = Global.Penguin_PA.My.Resources.Resources.Back
+        Me.PicPenguin.Image = Global.Penguin_PA.My.Resources.Resources.Intro_1
+        Me.PicPenguin.Location = New System.Drawing.Point(-3, -2)
+        Me.PicPenguin.Name = "PicPenguin"
+        Me.PicPenguin.Size = New System.Drawing.Size(2616, 834)
+        Me.PicPenguin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PicPenguin.TabIndex = 0
+        Me.PicPenguin.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 31.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(2612, 835)
+        Me.Controls.Add(Me.PicPenguin)
+        Me.Name = "Form1"
         Me.Text = "Form1"
-    End Sub
+        CType(Me.PicPenguin, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
 
+    End Sub
+    Friend WithEvents tmrAnimation As Timer
+    Friend WithEvents PicPenguin As PictureBox
 End Class
--- Penguin PA/Form1.vb
+++ Penguin PA/Form1.vb
@@ -1,3 +1,42 @@
 ﻿Public Class Form1
+    Dim PengCount As Integer = 1
+    Private Sub tmrAnimation_Tick(sender As Object, e As EventArgs) Handles tmrAnimation.Tick
+        Select Case PengCount
+            Case 1
+                PicPenguin.Image = My.Resources.Intro_1
+                PengCount += 1
+            Case 2
+                PicPenguin.Image = My.Resources.Intro_2
+                PengCount += 1
+            Case 3
+                PicPenguin.Image = My.Resources.Intro_3
+                PengCount += 1
+            Case 4
+                PicPenguin.Image = My.Resources.Intro_4
+                PengCount += 1
+            Case 5
+                PicPenguin.Image = My.Resources.Intro_4
+                PengCount = 1
 
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tmrAnimation.Start()
+        Me.tmrAnimation.Interval = 3
+
+        PicPenguin.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub PicPenguin_Click(sender As Object, e As EventArgs) Handles PicPenguin.Click
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PicPenguin.Click
+
+    End Sub
 End Class
